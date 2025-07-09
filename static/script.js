4f110629cbb42ace77fbbf83d8a51c9b2e13a316
Lovro <lovroperaic@gmail.com>
2025-07-09 16:47:30 +0200

    Add proper error handling

--- static/script.js
+++ static/script.js
@@ -1,14 +1,49 @@
 const form = document.querySelector("form");
 
+
 document.addEventListener("DOMContentLoaded", () => {
     form.addEventListener("submit", handleSubmit);
 });
 
+
+function getErrorMessage(element) {
+    if (element.type !== "email") {
+        return document.querySelector(`#${element.name}-error`);
+    } else {
+        if (element.validity.valueMissing) {
+            return document.querySelector("#email-missing");
+        } else {
+            return document.querySelector("#email-invalid");
+        }
+    }
+}
+
+function handleElementError(element) {
+    const errorMessage = getErrorMessage(element);
+    errorMessage.removeAttribute("hidden");
+    
+    element.classList.add("invalid");
+    element.addEventListener("input", hideError);
+
+    function hideError() {
+        errorMessage.setAttribute("hidden", "");
+        element.classList.remove("invalid");
+        element.removeEventListener("input", hideError);
+    }
+}
+
+function handleFormError(e) {
+    for (let element of form) {
+        if (!element.checkValidity()) handleElementError(element);
+    }
+}
+
 function handleSubmit(e) {
     e.preventDefault();
-    if (form.checkValidity()) {
-        alert("Good");
-    } else {
-        alert("Bad!")
-    }
+    form.checkValidity() ? handleSuccess() : handleFormError(e);
+}
+
+function handleSuccess() {
+    form.reset();
+    alert("Good");
 }
